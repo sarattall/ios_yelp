69dad8665727a05a56e8b062e1a69b116a2cb159
Sarat Tallamraju <sarattallamraju@gmail.com>
2015-02-12 21:44:04 -0800

    outlets wired up

--- Yelp/BusinessTableViewCell.h
+++ Yelp/BusinessTableViewCell.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface BusinessTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *thumbnail;
+@property (strong, nonatomic) IBOutlet UILabel *title;
+@property (strong, nonatomic) IBOutlet UILabel *miles;
+@property (strong, nonatomic) IBOutlet UILabel *dollar;
+@property (strong, nonatomic) IBOutlet UIImageView *stars;
+@property (strong, nonatomic) IBOutlet UILabel *reviews;
+@property (strong, nonatomic) IBOutlet UILabel *address;
+@property (strong, nonatomic) IBOutlet UILabel *description;
 
 @end
